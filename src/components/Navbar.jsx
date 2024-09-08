763405142f80e5ca7492c743d3b98bc82905f5ce
BDK27 <briansam195@gmail.com>
2024-09-08 22:02:12 +0800

    新增fontawesome

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,45 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faBagShopping,
+  faBars,
+  faBolt,
+} from "@fortawesome/free-solid-svg-icons";
+
 function Navbar() {
+  const icons = [
+    {
+      name: faUser,
+      size: "lg",
+    },
+    {
+      name: faBagShopping,
+      size: "lg",
+    },
+    {
+      name: faBars,
+      size: "lg",
+    },
+  ];
+
   return (
     <>
       {/* 手機版 */}
       <div className="md:hidden">
         <div className="container p-3 mx-auto shadow-sm">
           <div className="flex items-center justify-between">
-            <h1 className="text-xl font-bold">TSHOP</h1>
-            <ul className="flex items-center justify-center">
-              <li>language</li>
-              <li>search</li>
-              <li>user</li>
-              <li>shop</li>
-              <li>bar</li>
+            <div className="flex items-center justify-center">
+              {/* <FontAwesomeIcon icon={faFeatherPointed} /> */}
+              <FontAwesomeIcon icon={faBolt} className="text-yellow-400" />
+              <h1 className="ml-1 text-xl font-bold">SKYVOLT</h1>
+            </div>
+
+            <ul className="flex items-center justify-center gap-5">
+              {icons.map((item, index) => (
+                <li key={index}>
+                  <FontAwesomeIcon icon={item.name} size={item.size} />
+                </li>
+              ))}
             </ul>
           </div>
         </div>
