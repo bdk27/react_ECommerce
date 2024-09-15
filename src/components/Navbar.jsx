3db7d6ee8089654dee3fd9c648d3b0e897c570ac
BDK27 <briansam195@gmail.com>
2024-09-15 21:04:05 +0800

    新增logo與subList

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,48 +1,86 @@
+import { useState } from "react";
+import logo from "@/assets/logo.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faUser,
   faBagShopping,
   faBars,
-  faBolt,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
   const icons = [
     {
       name: faUser,
-      size: "lg",
     },
     {
       name: faBagShopping,
-      size: "lg",
     },
     {
       name: faBars,
-      size: "lg",
     },
   ];
+  const subList = [
+    {
+      name: "首頁",
+    },
+    {
+      name: "所有商品",
+    },
+    {
+      name: "關於",
+    },
+    {
+      name: "聯絡我們",
+    },
+  ];
+
+  const [isClicked, setIsClicked] = useState(false);
 
   return (
     <>
       {/* 手機版 */}
       <div className="md:hidden">
-        <div className="container p-3 mx-auto shadow-sm">
+        <div className="container relative p-3 mx-auto shadow-sm">
           <div className="flex items-center justify-between">
+            {/* logo */}
             <div className="flex items-center justify-center">
-              {/* <FontAwesomeIcon icon={faFeatherPointed} /> */}
-              <FontAwesomeIcon icon={faBolt} className="text-yellow-400" />
-              <h1 className="ml-1 text-xl font-bold">SKYVOLT</h1>
+              <img src={logo} alt="logo" className="w-[30px]" />
+              <h1 className="ml-1 text-xl font-bold">
+                <span className="text-mainOrange">T</span>SHOP
+              </h1>
             </div>
-
+            {/* icons */}
             <ul className="flex items-center justify-center gap-5">
-              {icons.map((item, index) => (
-                <li key={index}>
-                  <FontAwesomeIcon icon={item.name} size={item.size} />
-                </li>
-              ))}
+              <li>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faBagShopping} size="lg" />
+              </li>
+              <li onClick={() => setIsClicked((prev) => !prev)}>
+                {isClicked ? (
+                  <FontAwesomeIcon icon={faXmark} size="xl" />
+                ) : (
+                  <FontAwesomeIcon icon={faBars} size="lg" />
+                )}
+              </li>
             </ul>
           </div>
         </div>
+
+        {/* subItem */}
+        <ul
+          className={`absoilate w-full left-0 top-0 z-10 divide-y ${
+            isClicked ? "block" : "hidden"
+          }`}
+        >
+          {subList.map((item, index) => (
+            <li key={index} className="p-3">
+              {item.name}
+            </li>
+          ))}
+        </ul>
       </div>
 
       {/* 電腦版 */}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainYellow: "#f0b825",
+        mainOrange: "#ff7232",
+        mainPurple: "#9698ff",
+      },
+    },
   },
   plugins: [],
 };
