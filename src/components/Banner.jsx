08a67160d4c8cb68e72cd7e7b616a7a471ce49a9
YCheng <briansam195@gmail.com>
2024-09-26 23:13:47 +0800

    新增swiper

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,38 @@
-import clothesBanner from "@/assets/clothesBanner.png";
+import banner1 from "@/assets/img/banner1.png";
+import banner2 from "@/assets/img/banner2.png";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "@/assets/css/swiperStyle.css";
 
 function Banner() {
   return (
     <>
-      <div className="z-0">
-        <img src={clothesBanner} alt="clothesBanner" className="w-full" />
-      </div>
+      <Swiper
+        modules={[Navigation, Pagination, A11y, Autoplay]}
+        spaceBetween={50}
+        slidesPerView={1} //每頁顯示幾張
+        navigation
+        Autoplay
+        pagination={{ clickable: true }}
+        a11y={true}
+        loop={true}
+        autoplay={{
+          delay: 3000, // 自動播放延遲 3 秒
+          disableOnInteraction: false, // 用戶交互後仍繼續自動播放
+        }}
+      >
+        <SwiperSlide>
+          <img src={banner1} alt="banner1" />
+        </SwiperSlide>
+        <SwiperSlide className="w-full">
+          <img src={banner2} alt="banner2" />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 }
