d0954d875c83167621491d22cc1e7fa016724cb7
YCheng <briansam195@gmail.com>
2024-09-28 23:06:09 +0800

    調整Service.jsx樣式

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,6 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faTruckFast,
   faGift,
+  faMoneyBill,
   faHeadset,
 } from "@fortawesome/free-solid-svg-icons";
 
@@ -10,38 +11,57 @@ function Service() {
     {
       icon: faTruckFast,
       title: "免運無限次",
-      content: "下單選擇指定超商取貨，滿額即享免運",
+      content: ["下單選擇指定超商取貨，滿額即享免運"],
     },
     {
       icon: faGift,
       title: "滿額贈品",
-      content: "滿5,000元送500刷卡金(需登錄，限量 200 名)",
+      content: [
+        "滿 2,000 元送 9 折現金抵用券，單筆最高贈 300 元",
+        // "滿 5,000 元送 500 刷卡金(限量 200 名)",
+      ],
+    },
+    {
+      icon: faMoneyBill,
+      title: "限時優惠",
+      content: ["指定商品輸入折扣碼，限時 75 折"],
     },
     {
       icon: faHeadset,
-      title: "24小時專員服務",
-      content: "單筆消費滿 NT$1,000 可折抵 NT$50",
+      title: "24/7",
+      content: ["24小時專員服務"],
     },
   ];
 
   return (
     <>
-      <div className="py-3">
-        <ul className="flex flex-col items-start justify-between gap-3 md:flex-row">
-          {list.map((item, index) => (
-            <li key={index} className="flex items-center justify-between gap-1">
-              <FontAwesomeIcon
-                icon={item.icon}
-                size="lg"
-                className="pr-3 text-red-400"
-              />
-              <div>
-                <h3 className="font-bold">{item.title}</h3>
-                <p className="text-sm">{item.content}</p>
-              </div>
-            </li>
-          ))}
-        </ul>
+      <div className="py-5 bg-lightBrown">
+        <div className="container mx-auto">
+          <ul className="flex flex-col items-start justify-between gap-3 md:flex-row">
+            {list.map((item, index) => (
+              <li
+                key={index}
+                className="flex items-center md:text-center md:block"
+              >
+                <FontAwesomeIcon
+                  icon={item.icon}
+                  size="lg"
+                  className="p-3 mx-3 bg-white rounded-full text-mainBrown"
+                />
+                <div>
+                  <h3 className="py-1 font-bold">{item.title}</h3>
+                  <ul>
+                    {item.content.map((subItem, subIndex) => (
+                      <li key={subIndex} className="text-sm md:text-base">
+                        {subItem}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
